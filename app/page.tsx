59b77e582dfeeebea9cdaf24c7c8c415aa03599b
Ettore Mihaili <ettoremihaili@gmail.com>
2023-12-07 11:30:18 +0100

    fix: hint on what tags are currently displayed added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import FeedGrid from '@/components/FeedGrid/FeedGrid'
 import SearchBox from '@/components/SearchBox'
 import { Suspense } from 'react'
-import type {SearchParams} from '@/lib/types'
+import type { SearchParams } from '@/lib/types'
 
 export default async function Home({
     searchParams
-} : {
+}: {
     searchParams: SearchParams
 }) {
-
+    let tags: string;
+    if (typeof searchParams.tags === 'string') {
+        tags = searchParams.tags.split(',').join(', ');
+    } else {
+        throw new Error('searchParams.tags is not a string');
+    }
     return (
         <main className="relative z-50 max-w-4xl mx-auto flex-col h-full px-4">
             <div className='flex flex-col justify-center items-center mt-16 md:mt-32 mb-8'>
@@ -17,6 +22,7 @@ export default async function Home({
             </div>
             <SearchBox />
             <Suspense>
+                <p className='mb-4 px-4'><span className='text-lg text-muted-foreground font-semibold'>Results for:</span> {tags}</p>
                 <FeedGrid tags={searchParams.tags as string} />
             </Suspense>
         </main>
